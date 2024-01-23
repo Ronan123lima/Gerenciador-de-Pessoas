b668af34c4f2e91229a4022a7bc2ff5ed956d636
Ronan Lima <ronanlimasilva@hotmail.com>
2024-01-23 19:30:12 -0300

    inicia projeto

---
+++ src/test/java/br/com/Attornatus/Gerenciador/de/Pessoas/GerenciadorDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Attornatus.Gerenciador.de.Pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
